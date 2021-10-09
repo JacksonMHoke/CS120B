dc37e70714b50c255d12739bb8b3e18f980851a6
root <root@DESKTOP-NE0U87P.localdomain>
2021-10-09 01:17:01 -0700

    Added first 2 exercises

--- turnin/jhoke001_lab2_part1.c
+++ turnin/jhoke001_lab2_part1.c
@@ -15,25 +15,20 @@
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-				 PORTC = 0x00;
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
 	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
 	unsigned char tmpC = 0x00;
-	unsigned char tmpD = 0x00;
-	unsigned char cntavail = 0x00;
 	while(1) {
 			// Read input
 			tmpA = PINA & 0x01; //holds A0 value
 			tmpB = PINA & 0x02; //hold A1 value
-			tmpC = PINA & 0x04;
-			tmpD = PINA & 0x08;
-			cntavail=0x00;
-			if (tmpA==0x00) cntavail++;
-			if (tmpB==0x00) cntavail++;
-			if (tmpC==0x00) cntavail++;
-			if (tmpD==0x00) cntavail++;
+			if (tmpA==0x01 && tmpB==0x00) {
+				tmpC=0x01;
+			} else {
+				tmpC=0x00;
+			}
 		// Write output
-		PORTC=cntavail;
+		PORTB = tmpC;	
 	}
 	return 0;
 }
